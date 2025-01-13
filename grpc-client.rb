61051f46836803448ca77c00adc6b54c9330fb43
Bhagwati Pandey <v-bpandey@expediagroup.com>
2025-01-14 01:22:18 +0530

    release of v1.0.0

--- grpc-client.rb
+++ grpc-client.rb
@@ -1,31 +1,45 @@
 class GrpcClient < Formula
-    desc "Homebrew Package for a GRPC client to query the server"
-    homepage "https://bhagwati-web.github.io/homebrew-grpc-client"
-    url "https://github.com/bhagwati-web/grpc-client/releases/download/0.0.2/grpc-client-0.0.2.jar"
-    sha256 "445b375ebe3eb92f19322234dd3ab0fa0bbe6e2ca87dcaf757f7979c8c398be6"
-    license "MIT"
+  desc "Homebrew Package for a GRPC client to query the server"
+  homepage "https://bhagwati-web.github.io/homebrew-grpc-client"
   
-    depends_on "openjdk@17"
-    depends_on "grpcurl"
+  version "1.0.0"
+  @@jar_file = "grpc-client-#{version}.jar"
+  @@server_url = "http://localhost:8080"
+  @@server_port = "50051"
 
-    def install
-      libexec.install "grpc-client-0.0.2.jar"
-      (bin/"grpc-start").write <<~EOS
-        #!/bin/bash
-        exec java -jar #{libexec}/grpc-client-0.0.2.jar start "$@"
-      EOS
-      (bin/"grpc-stop").write <<~EOS
-        #!/bin/bash
-        exec lsof -t -i:50051 | xargs kill -9 "$@"
-      EOS
-    end
-    
-    def post_install
-      system "echo", "================================================"
-      system "echo", "================================================"
-      system "echo", "GRPC Client installed. Use 'grpc-start' and 'grpc-stop' to manage the server. It will be available on http://localhost:50051"
-      system "echo", "================================================"
-      system "echo", "================================================"
-    end
+  url "https://github.com/bhagwati-web/grpc-client/releases/download/#{version}/#{@@jar_file}"
+  sha256 "c42d886d16f1b6deadab3de714b94cfa75ad6fc9bd66312253aca7552fc16a17"
+  license "MIT"
+
+  depends_on "openjdk@17"
+  depends_on "grpcurl"
+
+  def install
+    libexec.install @@jar_file
+    (bin/"grpc-start").write <<~EOS
+      #!/bin/bash
+      exec java -jar #{libexec}/#{@@jar_file} start "$@"
+      
+      # Wait a moment for the server to start
+      sleep 5
+
+      # Open the default browser with the address
+      if command -v xdg-open > /dev/null; then
+        xdg-open "#{@@server_url}"
+      else
+        open "#{@@server_url}"
+      fi
+    EOS
+    (bin/"grpc-stop").write <<~EOS
+      #!/bin/bash
+      exec lsof -t -i:#{@@server_port} | xargs kill -9 "$@"
+    EOS
   end
   
+  def post_install
+    system "echo", "================================================"
+    system "echo", "GRPC Client installed. Use 'grpc-start' and 'grpc-stop' to manage the server."
+    system "echo", "The server will be available on #{@@server_url} and listening on port #{@@server_port}."
+    system "echo", "================================================"
+  end
+end
