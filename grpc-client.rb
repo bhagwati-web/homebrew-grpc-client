2f0178162e2c6a1cc514515c5a9009c8ecc5af4d
Bhagwati Pandey <v-bpandey@expediagroup.com>
2025-01-19 21:17:42 +0530

    reverted changes

--- grpc-client.rb
+++ grpc-client.rb
@@ -3,59 +3,49 @@ class GrpcClient < Formula
   homepage "https://bhagwati-web.github.io/homebrew-grpc-client"
   
   version "1.2.1"
-  jar_file = "grpc-client-#{version}.jar"
-  sha256 "7a8f8710de5756c30d2ab7406fc295390e48aa1718cbb7f0966db5125f594286"
-  server_port = "50051"
-  server_url = "http://localhost:#{server_port}"
+  @@jar_file = "grpc-client-#{version}.jar"
+  @@sha256 = "7a8f8710de5756c30d2ab7406fc295390e48aa1718cbb7f0966db5125f594286"
+  @@server_port = "50051"
+  @@server_url = "http://localhost:#{@@server_port}"
 
-  url "https://github.com/bhagwati-web/grpc-client/releases/download/#{version}/#{jar_file}"
-  sha256 sha256
+  url "https://github.com/bhagwati-web/grpc-client/releases/download/#{version}/#{@@jar_file}"
+  sha256 @@sha256
   license "MIT"
 
   depends_on "openjdk@17"
   depends_on "grpcurl"
 
   def install
-    libexec.install jar_file
-
+    libexec.install @@jar_file
     (bin/"grpcstart").write <<~EOS
       #!/bin/bash
-      set -e
-
-      # Set JAVA_HOME to use Java 17 installed via Homebrew
       export JAVA_HOME=$(/usr/libexec/java_home -v 17)
       export PATH=$JAVA_HOME/bin:$PATH
 
       # Start the GRPC client
-      exec java -jar #{libexec}/#{jar_file} start "$@"
+      exec java -jar #{libexec}/#{@@jar_file} start "$@"
       
       # Allow time for the server to start
       sleep 5
 
       # Open the default browser with the server URL
       if command -v xdg-open > /dev/null; then
-        xdg-open "#{server_url}"
+        xdg-open "#{@@server_url}"
       else
-        open "#{server_url}"
+        open "#{@@server_url}"
       fi
     EOS
-
     (bin/"grpcstop").write <<~EOS
       #!/bin/bash
-      set -e
-
-      # Stop the GRPC client running on the specified port
-      lsof -t -i:#{server_port} | xargs kill -9 "$@"
+      exec lsof -t -i:#{@@server_port} | xargs kill -9 "$@"
     EOS
   end
   
   def post_install
-    puts <<~EOS
-      \n\n\n\n========================================================
-      GRPC Client installed successfully.
-      Use 'grpcstart' to start the server and 'grpcstop' to stop it.
-      The server will be available at #{server_url}, listening on port #{server_port}.
-      ========================================================\n\n\n\n
-    EOS
+    system "echo", "\n\n\n================================================"
+    system "echo", "GRPC Client installed. Use 'grpcstart' and 'grpcstop' to manage the server."
+    system "echo", "If this doesn't work, please switch to java 17 and try again running above command."
+    system "echo", "The server will be available on #{@@server_url} and listening on port #{@@server_port}."
+    system "echo", "================================================\n\n\n"
   end
 end
